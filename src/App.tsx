33a4c224247693df3a4e3cd1400c4ed1b17d42d8
Jaime <jaime251102@gmail.com>
2022-03-29 16:03:46 -0400

    app updated

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,4 @@
+import { useEffect, useState } from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import './App.css';
 import { Nav } from './components/Nav';
@@ -6,14 +7,32 @@ import { Login } from './pages/Login';
 import { Register } from './pages/Register';
 
 function App() {
+
+  const [name, setName] = useState("")
+
+  useEffect(() => {
+    (
+     async () => {
+        const response = await fetch('http://localhost:8000/api/user',{
+          headers: {'Content-Type': 'application/json'},
+          credentials: 'include'
+        });
+        const content = await response.json();
+
+        setName(content.name);
+     }
+    )()
+  }, [])
+  
     return (
     <Router>
       <div className="App">
-        <Nav />
+        <Nav name={name} setName={setName} 
+        />
         <main className="form-signin">
             <Routes>
-              <Route path='/' element={<Home />} />
-              <Route path='/login' element={<Login />} />
+              <Route path='/' element={<Home name={name} />} />
+              <Route path='/login' element={<Login setName={setName} />} />
               <Route path='/register' element={<Register />} />
             </Routes>
         </main>
